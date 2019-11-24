eb636c7141e87abfdbd78099bd8abb20fd77cb16
DmitryKress <54477341+DmitryKress@users.noreply.github.com>
2019-11-24 13:46:17 +0200

    1
    1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import sum from './js/sum';
 import './styles/style.scss'
+import cities from './js/cities';
 console.log(sum(1,2))
 console.log('Proof that it works')
+
+console.log(cities);
+
+const locationSelect = document.getElementById('location-select');
+
+locationSelect.addEventListener('change', (event) => {
+  const city = event.target.value;
+  const defaulCity = 'img/skyline.jpg';
+  const cityImage = document.getElementById('location-img');
+
+  if(!cities[city]){
+    cityImage.src = defaulCity;
+    return;
+  };
+
+  cityImage.src = cities[city].url;
+});
+
